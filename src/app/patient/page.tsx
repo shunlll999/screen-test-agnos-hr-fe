50149062f49c687af7af00020b9920d53453a774
shunlll999 <rafael5715@gmail.com>
2026-07-25 10:11:09 +0700

    agnos-test(Added): initial project [AGN-001]

---
+++ src/app/patient/page.tsx
@@ -0,0 +1,9 @@
+const PatientPage = () => {
+  return (
+    <main className="min-h-screen px-4 py-10 sm:py-16">
+      Hello, this is the patient page.
+    </main>
+  );
+}
+
+export default PatientPage
---
+++ src/app/staff/page.tsx
@@ -0,0 +1,9 @@
+const StaffPage = () => {
+  return (
+    <main className="min-h-screen px-4 py-10 sm:py-16">
+      Hello, this is the staff page.
+    </main>
+  );
+}
+
+export default StaffPage
